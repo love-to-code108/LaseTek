e2ffa82a88872a54fff8fcbe0e839a23bae6d765
love-to-code108 <love.to.code108@gmail.com>
2025-01-19 15:38:05 +0530

    everything is going ok except for this annoying horizontal bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,126 @@
 import "./App.css";
 
+
+
+
+
+
+
+
+
 function App() {
+
+
+
+
+
+
+
+
+
   // DESKTOP VERSION    ==== MAX WIDTH = 1716 ====
   return (
     // THE MAIN GREY PAGE THE LOWEST LEVEL  ==MAIN 1==
-    <div className="main-1">
+    <div className="main-1 bg-gray-600 w-[100vw] h-[100vh] flex justify-center items-center z-0">
 
       {/* ==MAIN 2==  */}
-      <div className="main-2">
+      <div className="main-2 bg-white z-10 w-[100vw] max-w-[1716px]">
 
       
       {/* NAVIGATION BAR */}
-        <div>
+        <div className=" bg-red-600 w-[100%] h-[8rem]">
+
+
+        {/* LOGO */}
+          <div>
+            <img src="" alt="" />
+          </div>
+
+        {/* THE SECTIONS */}
+
+
+        {/* CONTACT US BUTTON */}
 
         </div>
 
+
+
+
       {/* HERO SECTION */}
+      <div className="w-[100%] h-[100vh] ">
+
+          {/* HERO SECTION WRITING SUPER BOX */}
+
+
+          {/* PICTURE 1 */}
+
+
+          {/* PICTURE 2 */}
+
+
+
+      </div>
+
+
 
       {/* ABOUT US SECTION */}
+        <div>
+
+          {/*ABOUT US SECTION WRITING SUPER BOX */}
+
+
+
+          {/* ABOUT US SECTION PICTURE 1 */}
+
+
+        </div>
+
+
+
 
       {/* SERVICES WE PROVIDE */}
+      <div>
+
+        {/* SERVICES SECTION WRITING SUPER BOX */}
+
+
+        {/* GRID FOR CARDS */}
+
+
+      </div>
+
+
+
 
       {/* WHY CHOOSE US */}
+      <div>
+
+
+        {/* WHY CHOOSE US WRITING SUPER BOX */}
+
+
+
+        {/* WHY CHOOSE US SECTION GRID FOR CARDS */}
+
+
+
+      </div>
+
 
       {/* CONTACT US */}
+      <div>
+
+
+        {/* CONTACT US HEADING */}
+
+      </div>
+
+
 
       {/* FOOTER */}
+      <div>
+
+      </div>
 
 
 
