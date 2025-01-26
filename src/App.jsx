82f51c5fe60770fa6fb15aaacd1d7d2505e2e6c2
love-to-code108 <love.to.code108@gmail.com>
2025-01-26 10:24:10 +0530

    starting from a clean slate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,43 @@
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./Home.jsx"
+import { HomePage } from "./Pages/Home.jsx"
 import { Gallery } from "./Pages/Gallery.jsx"
 
 
 function App(){
   return (
+    <div>
+      <Navbar/>
     <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/gallery" element={<Gallery/>}/>
     </Routes>
+    </div>
   )
+}
+
+
+
+
+// THE NAVBAR 
+const Navbar = () => {
+
+  return(
+    <div>
+      Navbar
+    </div>
+  );
+}
+
+
+
+// THE CONTACT US SECTION
+const ContactUs = () => {
+
+  return(
+    <div>
+      Contact Us Section
+    </div>
+  );
 }
 
 export default App
